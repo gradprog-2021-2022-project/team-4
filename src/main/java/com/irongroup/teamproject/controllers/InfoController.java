ac67c3f2b5d1ef50f8bdc5b0a332f8e8d6e4242d
Elias Neel <eliasneel2001@gmail.com>
2022-04-20 16:40:01 +0200

    info pagina voorbereiding

---
+++ src/main/java/com/irongroup/teamproject/controllers/InfoController.java
@@ -0,0 +1,12 @@
+package com.irongroup.teamproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InfoController {
+    @GetMapping({"/","/info"})
+    public String infopage(){
+        return "infopage";
+    }
+}

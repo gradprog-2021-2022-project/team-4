4c8a3eba7e576dd2e638fc0c2f75d434aeae36f5
Brent Budts <brent.budts@student.thomasmore.be>
2022-04-28 11:11:51 +0200

    merge detail en explore

--- src/main/java/com/irongroup/teamproject/controllers/PostController.java
+++ src/main/java/com/irongroup/teamproject/controllers/PostController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Collection;
+import java.util.Optional;
 
 @Controller
 public class PostController {
@@ -23,5 +25,14 @@ public class PostController {
     @GetMapping({"/foryoupage"})
     public String foryoupage(){
         return "foryoupage";
+    }
+    @GetMapping({"/postDetails/{id}","postDetails"})
+    public String postDetails(Model model, @PathVariable(required = false)Integer id){
+        if(id==null) return "postDetails";
+        Optional<FashPost> optionalFashPost = posts.findById(id);
+        if(optionalFashPost.isPresent()){
+            model.addAttribute("post", optionalFashPost.get());
+        }
+        return "postDetails";
     }
 }

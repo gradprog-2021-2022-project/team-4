6f0591e1d673e194255ccc041afc5d4b9c0e592c
Elias Neel <eliasneel2001@gmail.com>
2022-04-28 10:37:09 +0200

    explore aanmaken

--- src/main/java/com/irongroup/teamproject/controllers/PostController.java
+++ src/main/java/com/irongroup/teamproject/controllers/PostController.java
@@ -1,12 +1,23 @@
 package com.irongroup.teamproject.controllers;
 
+import com.irongroup.teamproject.model.FashPost;
+import com.irongroup.teamproject.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
 
 @Controller
 public class PostController {
+    @Autowired
+    PostRepository posts;
+
     @GetMapping({"/explorepage"})
-    public String explorepage(){
+    public String explorepage(Model model){
+        Collection<FashPost> postsmade=posts.findAll();
+        model.addAttribute("fashposts",postsmade);
         return "explorepage";
     }
     @GetMapping({"/foryoupage"})
--- src/main/java/com/irongroup/teamproject/repositories/PostRepository.java
+++ src/main/java/com/irongroup/teamproject/repositories/PostRepository.java
@@ -3,5 +3,8 @@ package com.irongroup.teamproject.repositories;
 import com.irongroup.teamproject.model.FashPost;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+
 public interface PostRepository extends CrudRepository<FashPost,Integer> {
+    Collection<FashPost> findAll();
 }

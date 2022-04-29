2d50d910f190f3f3de171741c7010af068735bb8
matthewds02 <descheppermatthew@gmail.com>
2022-04-29 16:47:31 +0200

    profilepage

--- src/main/java/com/irongroup/teamproject/controllers/UserController.java
+++ src/main/java/com/irongroup/teamproject/controllers/UserController.java
@@ -1,12 +1,26 @@
 package com.irongroup.teamproject.controllers;
 
+import com.irongroup.teamproject.model.FashUser;
+import com.irongroup.teamproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 public class UserController {
-    @GetMapping({"/profilepage"})
-    public String profilepage(){
+    @Autowired
+    UserRepository users;
+    @GetMapping({"/profilepage/{id}", "profilepage"})
+    public String profilepage(Model model, @PathVariable(required = false)Integer id){
+        if(id==null) return "profilepage";
+        Optional<FashUser> optionalFashUser = users.findById(id);
+        if(optionalFashUser.isPresent()){
+            model.addAttribute("user", optionalFashUser.get());
+        }
         return "profilepage";
     }
 }

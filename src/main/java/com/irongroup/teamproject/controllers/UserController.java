4223ea1f3ece0f5258e8cad666a8c3f212654d2d
brent <brent.budts11@gmail.com>
2022-05-01 19:24:28 +0200

    inlog en uitlog code

--- src/main/java/com/irongroup/teamproject/controllers/UserController.java
+++ src/main/java/com/irongroup/teamproject/controllers/UserController.java
@@ -1,12 +1,42 @@
 package com.irongroup.teamproject.controllers;
 
+import com.irongroup.teamproject.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository userRepository;
+
+
     @GetMapping({"/profilepage"})
     public String profilepage(){
         return "profilepage";
+    }
+
+    @GetMapping({"/loginerror"})
+    public String loginerror(){
+        return "loginerror";
+    }
+
+    @GetMapping("/logout")
+    public String logout(Principal principal, Model model) {
+        if (principal == null) return "redirect:/";
+        return "user/logout";
+    }
+
+    @GetMapping("/login")
+    public String login(Principal principal){
+        if(principal!=null) return "redirect:/";
+        return "user/login";
     }
 }

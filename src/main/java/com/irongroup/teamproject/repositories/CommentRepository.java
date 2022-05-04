c7ef97c02ef5fd684cd5633ff5859924487a222e
Elias Neel <eliasneel2001@gmail.com>
2022-05-04 15:06:16 +0200

    sorteren comments

--- src/main/java/com/irongroup/teamproject/repositories/CommentRepository.java
+++ src/main/java/com/irongroup/teamproject/repositories/CommentRepository.java
@@ -12,6 +12,6 @@ import java.util.Collection;
 public interface CommentRepository extends CrudRepository<FashComment,Integer> {
     Collection<FashComment> findAll();
 
-    @Query("select c from FashComment c where (c.post = :post)")
+    @Query("select c from FashComment c where (c.post = :post) order by c.date desc")
     Collection<FashComment> findCommentsForPost(@Param("post") FashPost post);
 }

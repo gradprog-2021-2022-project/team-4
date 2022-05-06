2d3a8fb81a5c5ed0d952c28b5c5926bd68ce7fb8
Elias Neel <eliasneel2001@gmail.com>
2022-05-06 09:59:05 +0200

    controller uitwerken clothingitem

--- src/main/java/com/irongroup/teamproject/controllers/ClothingController.java
+++ src/main/java/com/irongroup/teamproject/controllers/ClothingController.java
@@ -1,7 +1,28 @@
 package com.irongroup.teamproject.controllers;
 
+import com.irongroup.teamproject.model.Clothing_Item;
+import com.irongroup.teamproject.repositories.ClothingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class ClothingController {
+    @Autowired
+    ClothingRepository clothingRepository;
+
+    @GetMapping({"/clothingdetail", "/clothingdetail/{id}"})
+    public String clothingdetails(Model model, @PathVariable(required = false) Integer id) {
+        try {
+            if (id != null) {
+                Clothing_Item item = clothingRepository.findById(id).get();
+                model.addAttribute("item", item);
+            }
+        } catch (Exception e) {
+            System.out.println("geen clothing item");
+        }
+        return "clothing_detail";
+    }
 }

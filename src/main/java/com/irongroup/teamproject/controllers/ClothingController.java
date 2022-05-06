668b8cfb0364c4b4e5dc85994b284c0fdeaba795
Elias Neel <eliasneel2001@gmail.com>
2022-05-06 10:37:59 +0200

    controller implementeren voor te kunnen opslaan/verwijderen

--- src/main/java/com/irongroup/teamproject/controllers/ClothingController.java
+++ src/main/java/com/irongroup/teamproject/controllers/ClothingController.java
@@ -1,20 +1,26 @@
 package com.irongroup.teamproject.controllers;
 
 import com.irongroup.teamproject.model.Clothing_Item;
+import com.irongroup.teamproject.model.FashUser;
 import com.irongroup.teamproject.repositories.ClothingRepository;
+import com.irongroup.teamproject.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.security.Principal;
+
 @Controller
 public class ClothingController {
     @Autowired
     ClothingRepository clothingRepository;
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping({"/clothingdetail", "/clothingdetail/{id}"})
-    public String clothingdetails(Model model, @PathVariable(required = false) Integer id) {
+    public String clothingdetails(Model model, @PathVariable(required = false) Integer id, Principal principal) {
         try {
             if (id != null) {
                 Clothing_Item item = clothingRepository.findById(id).get();
@@ -22,7 +28,20 @@ public class ClothingController {
             }
         } catch (Exception e) {
             System.out.println("geen clothing item");
+            return "redirect:/";
+        }
+        if(principal!=null){
+            FashUser user=userRepository.findFashUserByUsername(principal.getName());
+            model.addAttribute("fashuser",user);
         }
         return "clothing_detail";
+    }
+    @GetMapping("/saveItem/{id}")
+    public String saveItem(@PathVariable Integer id){
+        return "redirect:/clothingdetail/"+id;
+    }
+    @GetMapping("/removeItem/{id}")
+    public String removeItem(@PathVariable Integer id){
+        return "redirect:/clothingdetail/"+id;
     }
 }

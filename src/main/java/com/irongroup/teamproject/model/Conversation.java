ff68e013db4c7dc3055b77c813d08fd837ff1603
AlleeElias <eliasneel2001@gmail.com>
2022-05-14 22:27:01 +0200

    uitwerken modellen 1

---
+++ src/main/java/com/irongroup/teamproject/model/Conversation.java
@@ -0,0 +1,16 @@
+package com.irongroup.teamproject.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Collection;
+
+@Entity
+public class Conversation {
+    @Id
+    private Integer id;
+
+    //Berichten die tot deze conversatie horen
+    @OneToMany(mappedBy = "conversation")
+    private Collection<Message> messages;
+}
--- src/main/java/com/irongroup/teamproject/model/Message.java
+++ src/main/java/com/irongroup/teamproject/model/Message.java
@@ -2,12 +2,25 @@ package com.irongroup.teamproject.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Message {
     @Id
     Integer id;
 
+    //Bij welke conversatie hoort dit bericht?
+    @ManyToOne(optional = false)
+    private Conversation conversation;
+
     public Message() {
+    }
+
+    public Conversation getConversation() {
+        return conversation;
+    }
+
+    public void setConversation(Conversation conversation) {
+        this.conversation = conversation;
     }
 }

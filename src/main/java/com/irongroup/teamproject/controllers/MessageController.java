091be4b5b6f47e71170cb5656996072e28cc58dd
AlleeElias <eliasneel2001@gmail.com>
2022-05-15 12:52:13 +0200

    uitwerken pagina

--- src/main/java/com/irongroup/teamproject/controllers/MessageController.java
+++ src/main/java/com/irongroup/teamproject/controllers/MessageController.java
@@ -1,5 +1,6 @@
 package com.irongroup.teamproject.controllers;
 
+import com.irongroup.teamproject.model.Conversation;
 import com.irongroup.teamproject.model.FashUser;
 import com.irongroup.teamproject.repositories.ConversationRepository;
 import com.irongroup.teamproject.repositories.UserRepository;
@@ -33,17 +34,22 @@ public class MessageController {
         }
     }
 
-    // TODO : voeg een check toe om te zien of de gebruiker toegang heeft tot de convo!!
+    // done : voeg een check toe om te zien of de gebruiker toegang heeft tot de convo!!
     @GetMapping("/messages/{id}")
     public String conversation(Principal p, Model model, @PathVariable Integer id) {
         //In een try catch= geen ifke
         try {
             //Gebruiker vinden die ingelogd is
             FashUser loggedIn = users.findFashUserByUsername(p.getName());
-            //Convo vinden die gevraagd worden
-            model.addAttribute("convo",convos.findbyID(id));
-            model.addAttribute("loggedUser",loggedIn);
-            return "user/conversation";
+            //Convo vinden die gevraagd word
+            Conversation conversation=convos.findbyID(id);
+            if(conversation.getUsers().contains(loggedIn)){
+                //Toevoegen aan het model enkel als de gebruiker toegang heeft tot de convo
+                model.addAttribute("convo",conversation);
+                model.addAttribute("loggedUser",loggedIn);
+                return "user/conversation";
+            }
+            else {return "redirect:/explorepage";}
         } catch (Exception e) {
             return "redirect:/explorepage";
         }
